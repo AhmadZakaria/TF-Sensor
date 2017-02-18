fed43102d44404202e4c387511b1170be52d5b1d
Ahmad Zakaria <ahmadz1991@gmail.com>
2017-02-18 09:37:38 +0100

    using real sensors

--- _sensors-project/sensor-server/lib/APIRouter.js
+++ _sensors-project/sensor-server/lib/APIRouter.js
@@ -3,7 +3,7 @@
 const parser = require("body-parser");
 const Sensors = require("./routes/sensors");
 const DefaultRouter = require("./DefaultRouter");
-//const TFSensor = require('../../tf-sensor/lib/TFSensor');
+const TFSensor = require('../../tf-sensor/lib/TFSensor');
 const PhoneSensor = require('../../phone-sensor/lib/PhoneSensor');
 const DummySensor = require('../../dummy-sensor/lib/DummySensor');
 
@@ -12,7 +12,7 @@ module.exports = class APIRouter extends require("express").Router {
     constructor(opts) {
         super(opts || APIRouter.defaultOptions());
 
-        this._sensors = new Sensors(DummySensor,DummySensor); //Mocked to use DummySensor :-) for testing at home
+        this._sensors = new Sensors(TFSensor,PhoneSensor); //Mocked to use DummySensor :-) for testing at home
 
         this.all("/sensors/:sensor/sensorReadings/latest", DefaultRouter.xPoweredBy,
             parser.json({
