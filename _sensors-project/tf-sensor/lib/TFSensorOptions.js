9b5d800a8d88e851385894768ef7e7337a50036b
Ahmad Zakaria <ahmadz1991@gmail.com>
2017-02-18 09:40:18 +0100

    removed prefixes

--- _sensors-project/tf-sensor/lib/TFSensorOptions.js
+++ _sensors-project/tf-sensor/lib/TFSensorOptions.js
@@ -6,11 +6,11 @@ var TFSensorOptions = {
     temperatureSensorOptions: {
         type: "Temperature Sensor",
         //sensor constructor
-        ctor: Tinkerforge.BrickletTemperature,
+        ctor: BrickletTemperature,
         //sensor measurement normalization factor
         normFact: 100.0,
         //sensor callback event type
-        callbackEvent: Tinkerforge.BrickletTemperature.CALLBACK_TEMPERATURE,
+        callbackEvent: BrickletTemperature.CALLBACK_TEMPERATURE,
         //sensor callback period function type
         frequency: 500,
         // periodic callback function name
@@ -28,11 +28,11 @@ var TFSensorOptions = {
     ambientLightSensorOptions: {
         type: "Ambient Sensor",
         //sensor constructor
-        ctor: Tinkerforge.BrickletAmbientLightV2,
+        ctor: BrickletAmbientLightV2,
         //sensor measurement normalization factor
         normFact: 100.0,
         //sensor callback event type
-        callbackEvent: Tinkerforge.BrickletAmbientLightV2.CALLBACK_ILLUMINANCE,
+        callbackEvent: BrickletAmbientLightV2.CALLBACK_ILLUMINANCE,
         //sensor callback period function type
         frequency: 500,
         // callback function name
@@ -46,11 +46,11 @@ var TFSensorOptions = {
     humiditySensorOptions: {
         type: "Humidity Sensor",
         //sensor constructor
-        ctor: Tinkerforge.BrickletHumidity,
+        ctor: BrickletHumidity,
         //sensor measurement normalization factor
         normFact: 10.0,
         //sensor callback event type
-        callbackEvent: Tinkerforge.BrickletHumidity.CALLBACK_HUMIDITY,
+        callbackEvent: BrickletHumidity.CALLBACK_HUMIDITY,
         //sensor callback period function type
         frequency: 500,
         // callback function name
@@ -63,11 +63,11 @@ var TFSensorOptions = {
     soundSensorOptions: {
         type: "Sound Intensity Sensor",
         //sensor constructor
-        ctor: Tinkerforge.BrickletSoundIntensity,
+        ctor: BrickletSoundIntensity,
         //sensor measurement normalization factor
         normFact: 1.0,
         //sensor callback event type
-        callbackEvent: Tinkerforge.BrickletSoundIntensity.CALLBACK_INTENSITY,
+        callbackEvent: BrickletSoundIntensity.CALLBACK_INTENSITY,
         //sensor callback period function type
         frequency: 500,
         // callback function name
