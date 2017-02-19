e5b2622ff80da20ad4f0a9a32358724a0f99535b
Ahmad Zakaria <ahmadz1991@gmail.com>
2017-02-19 19:26:19 +0100

    send server ip to client

--- _sensors-project/sensor-server/lib/routes/dashboard.js
+++ _sensors-project/sensor-server/lib/routes/dashboard.js
@@ -1,9 +1,26 @@
 "use strict";
 
-module.exports = function (request, response)
-{
+var os = require('os');
+
+
+
+
+module.exports = function (request, response) {
+    let interfaces = os.networkInterfaces();
+    let addresses = [];
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
+        }
+    }
+    console.log(addresses[0]);
+
     response.render("dashboard", {
         "menu_name": "dashboard",
-        "header": "Sensors Dashboard"
+        "header": "Sensors Dashboard",
+        "wsIP": addresses[0]
     });
 };
