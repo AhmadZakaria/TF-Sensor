4ae9b43c56a9b2a41adbaa45f8c18ceb59c2f4c5
valentina <osipova.valya08@gmail.com>
2017-03-12 20:25:46 +0100

    updated settings

--- _mobile/ionicPonyApp/src/app/app.module.ts
+++ _mobile/ionicPonyApp/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
+import { ControlPanel } from '../pages/controlPanel/controlPanel';
 import { ContactPage } from '../pages/contact/contact';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    ControlPanel,
     ContactPage,
     TabsPage
   ],
@@ -18,7 +18,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    ControlPanel,
     ContactPage,
     TabsPage
   ],
